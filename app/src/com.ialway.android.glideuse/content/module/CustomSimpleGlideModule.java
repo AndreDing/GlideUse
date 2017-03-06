3d66643ef9b5f9d566656c0d1f4f21851d3ab647
AndreDing <gmdingchao@gmail.com>
2017-03-06 11:53:14 +0800

    Author: DingChao Date: 2017/03/06 Function Or Bug: add volley module test Detail Describe:NU Note:NU Bugs bring in from this commit:NU

--- app/src/com.ialway.android.glideuse/content/module/CustomSimpleGlideModule.java
+++ app/src/com.ialway.android.glideuse/content/module/CustomSimpleGlideModule.java
@@ -4,7 +4,7 @@ import android.content.Context;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
-import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
+import com.bumptech.glide.load.engine.cache.ExternalCacheDiskCacheFactory;
 import com.bumptech.glide.module.GlideModule;
 
 /**
@@ -12,10 +12,16 @@ import com.bumptech.glide.module.GlideModule;
  */
 public class CustomSimpleGlideModule implements GlideModule {
 
+    private static final String CACHE_DIR = "custom_module_dir";
+    private static final int CHAHE_MAX_SIXE = 10 * 1024 * 1024;
+
     @Override
     public void applyOptions(Context context, GlideBuilder builder) {
 
-        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, "custom_module_dir", 10 * 1024 * 1024));
+        // /data/data/com.ialway.android.glideuse/cache/custom_module_dir
+//        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, CACHE_DIR, CHAHE_MAX_SIXE));
+        // /sdcard/Android/data/com.ialway.android.glideuse/cache/custom_module_dir
+        builder.setDiskCache(new ExternalCacheDiskCacheFactory(context, CACHE_DIR, CHAHE_MAX_SIXE));
     }
 
     @Override
